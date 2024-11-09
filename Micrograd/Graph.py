bb838ce734f44e25fedadbd449a6e8cee157cfaf
Phạm Anh Tuấn <23020704@vnu.edu.vn>
2024-11-09 12:02:04 +0700

    Update file Graph

--- Micrograd/Graph.py
+++ Micrograd/Graph.py
@@ -1,23 +1,26 @@
 from graphviz import Digraph
+from Micrograd.Micrograd import Value
 
 class Graph:
-    def __init__(self, root) -> None:
+    def __init__(self, root: Value) -> None:
         self.root = root
     
     def trace(self) -> tuple:
         nodes, edges = set(), set()
-        def build_trace(node):
+        def build_trace(node: Value) -> None:
             if node not in nodes:
                 nodes.add(node)
                 for node_children in node._prev:
                     edges.add((node_children, node))
                     build_trace(node_children)
+                    
         build_trace(self.root)
         return nodes, edges
     
-    def draw(self, format='svg', rankdir = 'LR') -> function:
+    def draw(self, format='svg', rankdir = 'LR') -> Digraph:
         assert rankdir in ['LR', 'TB'], 'rankdir must be one of {LR, TB}'
         nodes, edges = self.trace()
+        
         dot = Digraph(format = format, graph_attr = {'rankdir': rankdir})
         
         for node in nodes:
